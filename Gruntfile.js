4a9d11b173d767a78e03b3f299d33760c691ad5f
dsteinel <dimitri.steinel@gmail.com>
2015-04-16 13:03:49 +0200

    smaller grid.

--- Gruntfile.js
+++ Gruntfile.js
@@ -209,7 +209,7 @@ module.exports = function (grunt) {
         sassDir: '<%= yeoman.app %>/styles',
         cssDir: '.tmp/styles',
         generatedImagesDir: '.tmp/images/generated',
-        imagesDir: '<%= yeoman.app %>/images',
+        imagesDir: '<%= yeoman.app %>/images/{,*/}*',
         javascriptsDir: '<%= yeoman.app %>/scripts',
         fontsDir: '<%= yeoman.app %>/styles/fonts',
         importPath: './bower_components',
